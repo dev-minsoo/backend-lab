bc2f3cb64fe427b858b5c77df14cbe6b42c35292
dev-minsoo <minsoo010@naver.com>
2026-03-24 22:52:52 +0900

    fix(online-qna-service): mysql replica 복제 설정 보완

--- topics/online-qna-service/implementation/mysql/primary/init/01-create-replication-user.sql
+++ topics/online-qna-service/implementation/mysql/primary/init/01-create-replication-user.sql
@@ -1,4 +1,5 @@
 CREATE USER IF NOT EXISTS 'repl_user'@'%' IDENTIFIED WITH mysql_native_password BY 'repl_password';
+CREATE USER IF NOT EXISTS 'qna'@'%' IDENTIFIED WITH mysql_native_password BY 'qna';
 GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'repl_user'@'%';
 GRANT ALL PRIVILEGES ON qna_primary.* TO 'qna'@'%';
 FLUSH PRIVILEGES;
